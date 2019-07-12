d346aad1f975724d5ad54e7b41a518c776af7689
Vasiliy <vasyasjob@gmail.com>
2019-07-13 00:23:22 +0300

    added slider solution

---
+++ slider/js/main.js
@@ -0,0 +1,58 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider__item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrapper = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot'),
+        dotActiveName = 'dot-active';
+
+    slider(slideIndex, slides, prev, next, dotsWrapper, dots, dotActiveName);
+
+    function slider(slideIndex, slides, prev, next, dotsWrapper, dots, dotActiveName) {
+
+        showSlides(slideIndex);
+    
+        function showSlides(n) {
+    
+            if (n > slides.length) {
+                slideIndex = 1;
+            } 
+            if (n < 1) {
+                slideIndex = slides.length;
+            }
+            slides.forEach((item) => item.style.display = 'none');
+    
+            dots.forEach((item) => item.classList.remove(dotActiveName));
+    
+            slides[slideIndex - 1].style.display = 'block';
+            dots[slideIndex - 1].classList.add(dotActiveName);
+        }
+    
+        function increaseSlideIndex(n) {
+            showSlides(slideIndex += n);
+        }
+    
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+    
+        prev.addEventListener('click', () => {
+            increaseSlideIndex(-1);
+        });
+        
+        next.addEventListener('click', () => {
+            increaseSlideIndex(1);
+        });
+    
+        dotsWrapper.addEventListener('click', (event) => {
+            for (let i = 0; i < dots.length; i++) {
+                if (event.target.classList.contains('dot') &&
+                event.target == dots[i]) {
+                    currentSlide(++i);
+                }
+            }
+        });
+    }
+});
